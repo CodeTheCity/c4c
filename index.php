8669adbe0c16294ba6ff40d5a39f0cb57eac5167
maeyoko <mae.yoko@yahoo.com>
2015-06-20 15:59:08 +0100

    the real thing

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 	$skillsQuery = implode($skills, ',');
 	$query = "SELECT * FROM users, links, services WHERE users.id=links.uid AND services.id=links.sid AND services.name IN ($skillsQuery)";
 	$res = mysqli_query("SELECT * FROM users WHERE users.id=links.uid AND services.id=links.sid AND services.name IN ($skillsQuery)");
-	$results = "";
 	while($row = mysql_fetch_assoc($res))
 	{
 		echo $row["name"];
@@ -58,82 +57,45 @@
 <img src="http://i62.tinypic.com/23kpx0z.png" width="100%">
 
     
-    <h1>My first Bootstrap website!</h1>
+    <h1>Welcome to cyclists4cyclists!</h1>
 <br>
  
 <div id="demo"></div>
 
   <div class="row">
-<div class="col-md-3">
+<div class="col-md-5">
       <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="#">Home</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Menu 1 <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Submenu 1-1</a></li>
-            <li><a href="#">Submenu 1-2</a></li>
-            <li><a href="#">Submenu 1-3</a></li>                        
-          </ul>
-        </li>
-        <li><a href="#">Menu 2</a></li>
-        <li><a href="#">Menu 3</a></li>
+        <li class="active"><a href="#">What you need!</a></li>
+            <div class="checkbox">
+      <label> <label class="checkbox-inline"><input type="checkbox" value="">Shower</label>
+<label class="checkbox-inline"><input type="checkbox" value="">Bike Toolkit</label>
+<label class="checkbox-inline"><input type="checkbox" value="">Air Pump</label> </label>
+    </div>
+    <div class="checkbox">
+      <label> <label class="checkbox-inline"><input type="checkbox" value="">Spare tire</label>
+<label class="checkbox-inline"><input type="checkbox" value="">Shelter</label>
+<label class="checkbox-inline"><input type="checkbox" value="">Hospitality</label> </label>
+    </div>
+    
       </ul>
     </div>
 
-    <div class="col-md-3"> 
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-    </div>
+    <div class="col-md-7">      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#">Find your cycle buddy!</a></li>
     
-    <div class="col-md-3">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-    <div class="col-md-3"> 
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    
-    <div class="clearfix visible-lg"></div>
-  </div>
-
-<div id="map-canvas"></div>
-<div class="container-fluid" id="main">
-  <div class="row">
-  	<div class="col-xs-8" id="left">
-    
-      <h2>Bootstrap Google Maps Demo</h2>
-      
-      <!-- item list -->
-      <div class="panel panel-default">
-        <div class="panel-heading"><a href="">Item heading</a></div>
-      </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-        Aliquam in felis sit amet augue.</p>
-      
-      <hr>
-      
-      <!-- /item list -->
-      
-      <p>
-      <a href="http://www.bootply.com/render/129229">Demo</a> | <a href="http://bootply.com/129229">Source Code</a>
-      </p>
-      
-      <hr> 
-        
-      <p>
-      <a href="http://bootply.com" target="_ext" class="center-block btn btn-primary">More Bootstrap Snippets on Bootply</a>
-      </p>
-        
-      <hr>      
+      </ul>
 
     </div>
-    <div class="col-xs-4"><!--map-canvas will be postioned here--></div>
-    
-  </div>
 
 </div>
 
 
 </body>
+
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+    </footer>
 </html>
 

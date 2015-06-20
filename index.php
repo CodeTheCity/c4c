1ee7e309f09615b086bf724786d498b724872446
olafs <olafs@222-98.nus.edu.sg>
2015-06-20 13:20:36 +0100

    c4c init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+	<title>Cyclists 4 Cyclists</title>
+</head>
+<body>
+	<div class="top">
+		<div class="logo">logo</div>
+		<div class="login">Login</div>
+	</div>	
+	<div class="map">map</div>
+</body>
+</html>
+<?
+	echo "Cyclists 4 Cyclists is a community service by cyclists for cyclists. Find anything you need on the road from the fellow cyclist near you.";
+
+?>
